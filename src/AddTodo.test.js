313aab0b4ec9a724b02654ce6f31cb5b8d972b9d
ashaginaw <alshaginaw@gmail.com>
2022-06-27 15:04:28 -0400

    Final Upload of project

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -16,28 +16,82 @@ afterEach(() => {
   container = null;
 });
 
-
-
-
- test('test that App component doesn\'t render dupicate Task', () => {
+test('test that App component doesn\'t render dupicate Task', () => { //finished
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "12/30/2023";
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  let check = screen.getAllByText(/History Test/i);
+  expect(check.length).toBe(1);
+  check = screen.getByText(/History Test/i);
+  const checkDate = screen.getByText(new Date(dueDate).toLocaleDateString());
+  expect(check).toBeInTheDocument();
+  expect(checkDate).toBeInTheDocument();
  });
 
- test('test that App component doesn\'t add a task without task name', () => {
+ test('test that App component doesn\'t add a task without task name', () => { //finished
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const name = "";
+  fireEvent.change(inputTask, { target: { value: name}});
+  fireEvent.click(element);
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
  });
 
  test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: null}});
+  fireEvent.click(element);
+  const check = screen.getByText(/You have no todo's left/i);
+  expect(check).toBeInTheDocument();
  });
 
 
-
- test('test that App component can be deleted thru checkbox', () => {
+ test('test that App component can be deleted thru checkbox', () => { //finished
   render(<App />);
- });
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "12/30/2023";
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const check = screen.getByText(/History Test/i);
+  expect(check).toBeInTheDocument();
+  const checkBox = screen.getByRole('checkbox');
+  fireEvent.click(checkBox);
+  const noCheck = screen.getByText(/You have no todo's left/i);
+  expect(noCheck).toBeInTheDocument();
+   });
 
 
  test('test that App component renders different colors for past due events', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "12/30/2023";
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const check = screen.getByText(/History Test/i);
+  const historyCheck = check.style.background
+  const checkDate = screen.getByText(new RegExp(dueDate, "i"));
+  expect(check).toBeInTheDocument();
+  expect(checkDate).toBeInTheDocument();
+  expect(historyCheck).not.toBe("white")
  });
